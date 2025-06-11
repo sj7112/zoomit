b1f2254a2d630e94c9fe95720b8041ee8ea81018
sj7112 <sj7112@126.com>
2025-06-11 21:11:43 +0800

    代码调整

--- bin/cos/init_centos.sh
+++ bin/cos/init_centos.sh
@@ -15,7 +15,20 @@
 # 使用示例:
 # sudo ./init_main.sh
 
-set -euo pipefail                                                # strict mode
+# 检查 DEBUG 环境变量，设置相应的调试模式
+
+if [[ "$DEBUG" == "1" ]]; then
+  # set -x          # 启用命令追踪
+  set -e          # 启用脚本错误即退出
+  set -u          # 启用未定义变量报错
+  set -o pipefail # 启用管道失败时退出
+else
+  set +x          # 关闭命令追踪
+  set -e          # 确保遇到错误退出
+  set -u          # 确保未定义变量时报错
+  set -o pipefail # 确保管道中的命令失败会导致脚本退出
+fi
+
 BIN_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd .. && pwd)" # bin directory
 source "$BIN_DIR/init_main.sh"                                   # main script
 
