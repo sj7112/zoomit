4d753782ee8dddf695b2b8d220e8d5e101eb0d8f
sj7112 <sj7112@126.com>
2025-06-27 11:24:28 +0800

    python_bridge调用的主程序移到根目录

--- lib/python_bridge.sh
+++ lib/python_bridge.sh
@@ -6,7 +6,7 @@ if [[ -z "${LOADED_PYTHON_BRIDGE:-}" ]]; then
 
   # Declare global
   : "${LIB_DIR:=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)}" # bin direcotry
-  : "${PYTHON_DIR:=$(dirname "$BIN_DIR")/python}"               # python directory
+  : "${ROOT_DIR:=$(dirname "$BIN_DIR")}"                        # project root directory
   : VENV_BIN="$HOME/.venv/bin/python"
 
   # python3虚拟环境（区分是否需要 sudo 包装）
@@ -21,36 +21,20 @@ if [[ -z "${LOADED_PYTHON_BRIDGE:-}" ]]; then
 
   # ===== 调用 myshell.py 中的命令 =====
   sh_update_source() {
-    py_exec "$PYTHON_DIR/myshell.py" sh_update_source "$DISTRO_OSTYPE"
+    py_exec "$ROOT_DIR/myshell.py" sh_update_source "$DISTRO_OSTYPE"
   }
 
   # User interaction, cannot use subshells like $(...), use configuration file to pass data
   sh_fix_ip() {
-    py_exec "$PYTHON_DIR/myshell.py" sh_fix_ip
+    py_exec "$ROOT_DIR/myshell.py" sh_fix_ip
   }
 
   sh_clear_cache() {
-    py_exec "$PYTHON_DIR/myshell.py" sh_clear_cache
+    py_exec "$ROOT_DIR/myshell.py" sh_clear_cache
   }
 
   # ===== 调用 mypip.py 中的命令 =====
   sh_install_pip() {
-    py_exec "$PYTHON_DIR/mypip.py"
+    py_exec "$ROOT_DIR/mypip.py"
   }
-
-  # ===== 从ast_parser.py导入的函数 =====
-  parse_code_files() {
-    local sh_file="$1"
-
-    # 直接输出处理结果
-    python3 -c "
-import sys
-sys.path.append('$PYTHON_DIR')
-from lang_util import parse_code_files
-
-for item in parse_code_files('$sh_file'):
-    print(item)
-"
-  }
-
 fi
